87fd523344e83df75bca4428a1906726285e8bf3
fabwe1998 <43571654+fabwe1998@users.noreply.github.com>
2018-10-30 19:13:50 +0530

    Add files via upload

---
+++ home/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+	# name=forms.CharField(max_length=100,required=True)
+	# email=forms.EmailField(max_length=254,help_text='eg. yourmail@anymail.com')
+
+	class Meta:
+		model=User
+		fields=('first_name','last_name','username','email','password1','password2')
